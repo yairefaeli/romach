1ed05f176ede24beee9fba1ef9c92bc0116566b0
Yairefaeli <yairefaeli324@gmail.com>
2024-11-12 22:59:43 +0200

    right structure

--- src/domain/entities/ProtectedFolderStatus.ts
+++ src/domain/entities/ProtectedFolderStatus.ts
@@ -1,6 +1,3 @@
-export type ProtedctedFolderErrorStatus =
-    | 'worng-password-for-folder'
-    | 'general-error'
-    | 'not-found'
-    | 'folder-not-protected ';
+export type FolderErrorStatus = 'worng-password-for-folder' | 'general-error' | 'not-found' | 'folder-not-protected ';
 
+export type ProtedctedFolderErrorStatus = 'worng-password-for-folder' | FolderErrorStatus;
