0c49f87377d63a8c51ae1ac837023a88388ea8b0
Yairefaeli <yairefaeli324@gmail.com>
2024-11-12 23:00:38 +0200

    more functions i need

--- src/application/interfaces/romach-entities-api.interface.ts
+++ src/application/interfaces/romach-entities-api.interface.ts
@@ -1,28 +1,21 @@
+import { FolderErrorStatus, ProtedctedFolderErrorStatus } from 'src/domain/entities/ProtectedFolderStatus';
 import { BasicFolder } from '../../domain/entities/BasicFolder';
 import { Hierarchy } from '../../domain/entities/Hierarchy';
-import { Result } from 'rich-domain';
-import { Folder } from 'src/domain/entities/Folder';
-import { ProtedctedFolderErrorStatus } from 'src/domain/entities/ProtectedFolderStatus';
 import { Timestamp } from 'src/domain/entities/Timestamp';
+import { Folder } from 'src/domain/entities/Folder';
+import { Result } from 'rich-domain';
 
 export interface RomachEntitiesApiInterface {
+    getFolderByIdWithPassword(folderId: string, Password: string): Promise<Result<Folder, ProtedctedFolderErrorStatus>>;
+    getFoldersByIdWithPassword(
+        input: { folderId: string; Password: string }[],
+    ): Promise<Result<Folder[], ProtedctedFolderErrorStatus>>;
+    getFolderByIdWithoutPassword(folderId: string): Promise<Result<Folder, FolderErrorStatus>>;
+    getFoldersByIdWithoutPassword(folderId: string[]): Promise<Result<Folder[], FolderErrorStatus>>;
 
-  getFolderByIdWithPassword(
-    folderId: string,
-    Password: string
-  ): Promise<Result<Folder, ProtedctedFolderErrorStatus>>;
+    getBasicFoldersByTimestamp(timestamp: Timestamp): Promise<Result<BasicFolder[]>>;
 
-  getFolderByIdWithoutPassword(
-    folderId: string
-  ): Promise<Result<Folder, ProtedctedFolderErrorStatus>>;
+    getHierarchies(): Promise<Result<Hierarchy[]>>;
 
-  getBasicFoldersByTimestamp(
-    timestamp: Timestamp
-  ): Promise<Result<BasicFolder[]>>;
-
-  getHierarchies(): Promise<Result<Hierarchy[]>>
-
-  checkPasswords(id: string, password: string): Promise<Result<boolean>>
+    checkPasswords(id: string, password: string): Promise<Result<boolean>>;
 }
-
-
