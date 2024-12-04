e15972f2d5e31d36eb4a78ebc704b60214184c05
Matan Azarzar <Mat528285@gmail.com>
2024-12-04 13:56:26 +0200

    fix bugs of yair

--- src/domain/entities/RegisteredFolderTypes.ts
+++ src/domain/entities/RegisteredFolderTypes.ts
@@ -1,27 +1,22 @@
-import { RegisteredFolderProps } from "./RegisteredFolder";
+import { RegisteredFolderProps } from './RegisteredFolder';
 
-export type RegisteredFolderErrorStatus =
-  | 'wrong-password'
-  | 'general-error'
-  | 'not-found'
-  | 'loading';
+export type RegisteredFolderErrorStatus = 'wrong-password' | 'general-error' | 'not-found' | 'loading';
 
 export type RegisteredFolderStatus = 'valid' | RegisteredFolderErrorStatus;
 
 export type ValidRegisteredFolderInput = Pick<
-  RegisteredFolderProps,
-  'upn' | 'folder' | 'password' | 'lastValidPasswordTimestamp'
+    RegisteredFolderProps,
+    'upn' | 'folder' | 'password' | 'lastValidPasswordTimestamp'
 >;
 
 export type InvalidRegisteredFolderInput = Pick<
-  RegisteredFolderProps,
-  'upn' | 'folderId' | 'isPasswordProtected' | 'password' | 'lastValidPasswordTimestamp' | 'status'
+    RegisteredFolderProps,
+    'upn' | 'folderId' | 'isPasswordProtected' | 'password' | 'lastValidPasswordTimestamp' | 'status'
 >;
 
 export type RegisteredFolderInput = ValidRegisteredFolderInput | InvalidRegisteredFolderInput;
 
-
 export type PasswordProtectedValidSpecificationProps = Pick<
-  RegisteredFolderProps,
-  'isPasswordProtected' | 'password' | 'lastValidPasswordTimestamp'
+    RegisteredFolderProps,
+    'isPasswordProtected' | 'password' | 'lastValidPasswordTimestamp'
 >;
